77646f09883f90ad6d52029a1d9039f13e50c412
EthanHiroz <162729379+EthanHiroz@users.noreply.github.com>
2024-04-19 15:12:12 +0000

    Submit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+temp = int(input("Enter the Current Temperature: "))
 
+choice = input("Type 1 for a Fahrenheit to Centigrade. Type 2 for Centigrade to Fahrenheit: ")
+
+if choice == "1":
+    centigrade = (5/9) * (temp - 32)
+    print(centigrade)
+
+elif choice == "2":
+    fahrenheit = (9*temp + (32 * 5))/5
+    print(fahrenheit)
+
+else:
+    print("Invaild Input")
